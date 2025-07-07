09cab6fc26da0784241b91ef96ee8812517e29b7
pratik.m21 <154410745+pratik-mhasawade@users.noreply.github.com>
2025-07-07 22:27:24 +0530

    Update pc.v

--- pc.v
+++ pc.v
@@ -1,1 +1,16 @@
+// src/core/pc.v
+module pc (
+    input  wire        clk,
+    input  wire        reset,
+    input  wire        pc_write_enable,
+    input  wire [31:0] pc_next,
+    output reg  [31:0] pc_current
+);
+    always @(posedge clk or posedge reset) begin
+        if (reset)
+            pc_current <= 32'h00000000;
+        else if (pc_write_enable)
+            pc_current <= pc_next;
+    end
+endmodule
 
